5a8679e4de74e1aa01e386185dd46fc4005333f4
FE-SW <ingal777@naver.com>
2024-11-02 18:10:23 +0900

    style:폰트파일 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
+/** @jsxImportSource @emotion/react */
+
+import { Global, css } from "@emotion/react";
 import AppRouter from "./routes/AppRouter";
 
+const GlobalStyles = css`
+  @font-face {
+    font-family: "Wanted Sans";
+    src: url("/fonts/WantedSans.ttf") format("truetype");
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: "Wanted Sans", sans-serif;
+  }
+`;
 
 function App() {
   return (
-    <AppRouter />
+    <>
+      <Global styles={GlobalStyles} />
+      <AppRouter />;
+    </>
   );
 }
 
