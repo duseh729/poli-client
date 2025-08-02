6b6f81fb2ad966190d429951cb22d8ee4c5dfa36
안도연 <duseh729@naver.com>
2025-08-02 15:37:27 +0900

    css: chat init css 수정

--- src/components/Chat/InfoCollection/index.tsx
+++ src/components/Chat/InfoCollection/index.tsx
@@ -91,7 +91,7 @@ const InfoCollection = ({
     };
 
     try {
-      await chatStream({requestBody, config: { }});
+      await chatStream({ requestBody, config: {} });
 
       const updatedRooms = await refetchChatRooms();
 
@@ -126,218 +126,189 @@ const InfoCollection = ({
       <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ko">
         <S.Form>
           <S.Title>
-            <S.Logo src={poliSmLogo} alt="POLI" />
             <S.TitleText>
               상담을 시작하기에 앞서, 사건에 대한 정보를 입력해 주세요.
             </S.TitleText>
           </S.Title>
-          <S.FormGroup>
-            <S.Label>
-              <S.Highlight>피해 장소 |</S.Highlight> 어디에서 피해를 겪으셨나요?
-              피해를 당하신 SNS, 혹은 사이트 명을 입력해 주세요.
-            </S.Label>
-            <S.Input
-              type="text"
-              placeholder="SNS 혹은 사이트명"
-              value={place}
-              onChange={(e) => setPlace(e.target.value)}
-            />
-          </S.FormGroup>
-          <S.FormGroup>
-            <S.Label>
-              <S.Highlight>피해 일시 |</S.Highlight> 언제 피해를 겪으셨나요?
-              날짜와 시간을 선택해 주세요.
-            </S.Label>
-            <S.InputRow>
-              <div>
-                <DatePicker
-                  value={selectedDate}
-                  onChange={(newDate) => setSelectedDate(newDate)}
-                  format="YYYY.MM.DD"
-                  slotProps={{
-                    textField: {
-                      placeholder: "날짜",
-                      inputProps: {
-                        style: {
-                          color: "#0F0F10",
-                          fontFamily: "Wanted Sans",
-                          fontSize: "14px",
-                          fontWeight: 500,
-                        },
-                      },
-                    },
-                    popper: {
-                      sx: {
-                        "& .MuiPaper-root": {
-                          width: "340px",
-                          borderRadius: "10px",
-                        },
-                      },
-                    },
-                  }}
-                  sx={{
-                    width: "340px",
-                    "& .MuiOutlinedInput-root": {
-                      height: "56px",
-                      backgroundColor: "#f6f8fb",
-                      borderRadius: "10px",
-                      "& fieldset": {
-                        borderColor: "#c0cbd9",
-                      },
-                      "&:hover fieldset": {
-                        borderColor: "#0059ff",
-                      },
-                      "&.Mui-focused fieldset": {
-                        border: "1px solid #0059ff",
-                      },
-                    },
-                  }}
-                />
-              </div>
-              <div>
-                <TimePicker
-                  value={selectedTime}
-                  format="a hh:mm"
-                  minutesStep={1}
-                  ampm={true}
-                  onChange={(newTime) => setSelectedTime(newTime)}
-                  slotProps={{
-                    textField: {
-                      placeholder: "시간",
-                      inputProps: {
-                        style: {
-                          color: "#0F0F10",
-                          fontFamily: "Wanted Sans",
-                          fontSize: "14px",
-                          fontWeight: 500,
-                        },
-                      },
-                    },
-                    popper: {
-                      sx: {
-                        "& .MuiPaper-root": {
-                          width: "340px",
-                          borderRadius: "10px",
-                        },
-                        "& .MuiMultiSectionDigitalClock-root": {
-                          display: "flex",
-                        },
-                        "& .MuiMultiSectionDigitalClockSection-root": {
-                          flex: "1 1 33.33%",
-                          textAlign: "center",
-                          overflowY: "auto",
-                          height: "200px",
-                          display: "flex",
-                          flexDirection: "column",
-                          alignItems: "center",
-                          "::-webkit-scrollbar": {
-                            width: "8px",
+          <S.FormGroupWrapper>
+            <S.FormGroup>
+              <S.Label>
+                <S.Highlight>피해 장소 |</S.Highlight> 어디에서 피해를
+                겪으셨나요? 피해를 당하신 SNS, 혹은 사이트 명을 입력해 주세요.
+              </S.Label>
+              <S.Input
+                type="text"
+                placeholder="SNS 혹은 사이트명"
+                value={place}
+                onChange={(e) => setPlace(e.target.value)}
+              />
+            </S.FormGroup>
+            <S.FormGroup>
+              <S.Label>
+                <S.Highlight>피해 일시 |</S.Highlight> 언제 피해를 겪으셨나요?
+                날짜와 시간을 선택해 주세요.
+              </S.Label>
+              <S.InputRow>
+                <div>
+                  <DatePicker
+                    value={selectedDate}
+                    onChange={(newDate) => setSelectedDate(newDate)}
+                    format="YYYY.MM.DD"
+                    slotProps={{
+                      textField: {
+                        placeholder: "날짜",
+                        inputProps: {
+                          style: {
+                            color: "#0F0F10",
+                            fontFamily: "Wanted Sans",
+                            fontSize: "14px",
+                            fontWeight: 500,
                           },
-                          "::-webkit-scrollbar-thumb": {
-                            backgroundColor: "#c0cbd9",
+                        },
+                      },
+                      popper: {
+                        sx: {
+                          "& .MuiPaper-root": {
+                            width: "340px",
                             borderRadius: "10px",
                           },
-                          "::-webkit-scrollbar-track": {
-                            background: "#f6f8fb",
+                        },
+                      },
+                    }}
+                    sx={{
+                      width: "340px",
+                      "& .MuiOutlinedInput-root": {
+                        height: "56px",
+                        backgroundColor: "#f6f8fb",
+                        borderRadius: "10px",
+                        "& fieldset": {
+                          borderColor: "#c0cbd9",
+                        },
+                        "&:hover fieldset": {
+                          borderColor: "#0059ff",
+                        },
+                        "&.Mui-focused fieldset": {
+                          border: "1px solid #0059ff",
+                        },
+                      },
+                    }}
+                  />
+                </div>
+                <div>
+                  <TimePicker
+                    value={selectedTime}
+                    format="a hh:mm"
+                    minutesStep={1}
+                    ampm={true}
+                    onChange={(newTime) => setSelectedTime(newTime)}
+                    slotProps={{
+                      textField: {
+                        placeholder: "시간",
+                        inputProps: {
+                          style: {
+                            color: "#0F0F10",
+                            fontFamily: "Wanted Sans",
+                            fontSize: "14px",
+                            fontWeight: 500,
                           },
                         },
-                        "& .Mui-selected": {
-                          backgroundColor: "#BDD5FF !important",
-                          color: "#0F0F10 !important",
-                          fontWeight: 600,
-                          fontFamily: "Wanted Sans",
-                          width: "100%",
-                          display: "block",
-                          padding: "8px 12px",
-                          boxSizing: "border-box",
-                          borderRadius: "7px",
+                      },
+                      popper: {
+                        sx: {
+                          "& .MuiPaper-root": {
+                            width: "340px",
+                            borderRadius: "10px",
+                          },
+                          "& .MuiMultiSectionDigitalClock-root": {
+                            display: "flex",
+                          },
+                          "& .MuiMultiSectionDigitalClockSection-root": {
+                            flex: "1 1 33.33%",
+                            textAlign: "center",
+                            overflowY: "auto",
+                            height: "200px",
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                            "::-webkit-scrollbar": {
+                              width: "8px",
+                            },
+                            "::-webkit-scrollbar-thumb": {
+                              backgroundColor: "#c0cbd9",
+                              borderRadius: "10px",
+                            },
+                            "::-webkit-scrollbar-track": {
+                              background: "#f6f8fb",
+                            },
+                          },
+                          "& .Mui-selected": {
+                            backgroundColor: "#BDD5FF !important",
+                            color: "#0F0F10 !important",
+                            fontWeight: 600,
+                            fontFamily: "Wanted Sans",
+                            width: "100%",
+                            display: "block",
+                            padding: "8px 12px",
+                            boxSizing: "border-box",
+                            borderRadius: "7px",
+                          },
+                          "& .MuiTypography-root": {
+                            fontSize: "22px",
+                            fontWeight: 500,
+                            color: "#0F0F10 !important",
+                            fontFamily: "Wanted Sans",
+                          },
                         },
-                        "& .MuiTypography-root": {
-                          fontSize: "22px",
-                          fontWeight: 500,
-                          color: "#0F0F10 !important",
-                          fontFamily: "Wanted Sans",
+                      },
+                    }}
+                    sx={{
+                      width: "340px",
+                      "& .MuiOutlinedInput-root": {
+                        height: "56px",
+                        backgroundColor: "#f6f8fb",
+                        borderRadius: "10px",
+                        "& fieldset": {
+                          borderColor: "#c0cbd9",
+                        },
+                        "&:hover fieldset": {
+                          borderColor: "#0059ff",
+                        },
+                        "&.Mui-focused fieldset": {
+                          border: "1px solid #0059ff",
                         },
                       },
-                    },
-                  }}
-                  sx={{
-                    width: "340px",
-                    "& .MuiOutlinedInput-root": {
-                      height: "56px",
-                      backgroundColor: "#f6f8fb",
-                      borderRadius: "10px",
-                      "& fieldset": {
-                        borderColor: "#c0cbd9",
-                      },
-                      "&:hover fieldset": {
-                        borderColor: "#0059ff",
-                      },
-                      "&.Mui-focused fieldset": {
-                        border: "1px solid #0059ff",
-                      },
-                    },
-                  }}
-                />
+                    }}
+                  />
+                </div>
+              </S.InputRow>
+            </S.FormGroup>
+            <S.FormGroup>
+              <S.Label>
+                <S.Highlight>피해 상세 상황 |</S.Highlight> 어떤 상황이고 어떤
+                부분이 고민되는지 설명해 주세요.
+              </S.Label>
+              <S.ExampleText>
+                예시. OO에게 계좌이체로 입금했는데, 약속한 물건을 아예 받지
+                못했어. 돈을 돌려받을 수 있을까?
+              </S.ExampleText>
+              <textarea
+                css={S.textAreaStyle(
+                  situationDescription.length >= DESCRIPTION_MAX_LENGTH
+                )}
+                value={situationDescription}
+                onChange={handleTextAreaChange}
+                rows={4}
+                placeholder="어떤 피해를 어떻게 경험했는지 편하게 입력해 주세요."
+              />
+              <div
+                css={S.charCountStyle(
+                  situationDescription.length === DESCRIPTION_MAX_LENGTH
+                )}
+              >
+                {situationDescription.length}/{DESCRIPTION_MAX_LENGTH}
               </div>
-            </S.InputRow>
-          </S.FormGroup>
-          <S.FormGroup>
-            <S.Label>
-              <S.Highlight>피해 상황 |</S.Highlight> 어떤 상황이고 어떤 부분이
-              고민되는지 설명해 주세요.
-            </S.Label>
-            <S.ExampleText>
-              예시. OO에게 계좌이체로 입금했는데, 약속한 물건을 아예 받지
-              못했어. 돈을 돌려받을 수 있을까?
-            </S.ExampleText>
-            <textarea
-              css={S.textAreaStyle(
-                situationDescription.length >= DESCRIPTION_MAX_LENGTH
-              )}
-              value={situationDescription}
-              onChange={handleTextAreaChange}
-              rows={4}
-              placeholder="어떤 피해를 어떻게 경험했는지 편하게 입력해 주세요."
-            />
-            <div
-              css={S.charCountStyle(
-                situationDescription.length === DESCRIPTION_MAX_LENGTH
-              )}
-            >
-              {situationDescription.length}/{DESCRIPTION_MAX_LENGTH}
-            </div>
-          </S.FormGroup>
-          <S.CheckBoxGroup>
-            <S.DuplicationWrapper>
-              <S.Duplication bold={true}>신고 중복 확인 &nbsp;|</S.Duplication>
-              <S.Duplication bold={false}>
-                &nbsp; 이미 신고가 이루어졌다면 아래 본인의 상황을 선택해 주세요
-              </S.Duplication>
-            </S.DuplicationWrapper>
-            <S.CheckBoxLabel>
-              <input
-                type="checkbox"
-                css={[S.hiddenCheckboxStyles, S.hiddenCheckboxCheckedStyles]}
-                checked={selectedCheckBox === 1}
-                onChange={() => handleCheckBoxChange(1)}
-              />
-              <span css={S.customCheckboxStyles} />
-              1. 해당 사건을 다른 경로를 통해(신문고, 오프라인 경찰서 등) 이미
-              신고했습니다.
-            </S.CheckBoxLabel>
-            <S.CheckBoxLabel>
-              <input
-                type="checkbox"
-                css={[S.hiddenCheckboxStyles, S.hiddenCheckboxCheckedStyles]}
-                checked={selectedCheckBox === 2}
-                onChange={() => handleCheckBoxChange(2)}
-              />
-              <span css={S.customCheckboxStyles} />
-              2. 해당 사건의 법적 절차가 이미 진행 중입니다. (법원 출석 등의
-              절차)
-            </S.CheckBoxLabel>
-          </S.CheckBoxGroup>
+            </S.FormGroup>
+          </S.FormGroupWrapper>
         </S.Form>
         <S.StartButton
           onClick={handleSubmit}
