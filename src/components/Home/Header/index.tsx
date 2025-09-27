771cf33c4565376f4a19ba68eb279c6e234ab529
doyeon <duseh729@naver.com>
2025-09-28 02:10:22 +0900

    feat: 온보딩 로고 모바일 이미지 수정

--- src/components/Home/Header/index.tsx
+++ src/components/Home/Header/index.tsx
@@ -1,31 +1,38 @@
 /** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react"; // css import 추가
+import { css } from "@emotion/react";
 import * as S from "./style";
 import poliText from "@/assets/poli-text.svg";
+import poliSmText from "@/assets/poli-text-sm.svg"; // 모바일 로고 import
 import { media } from "@/constants/media";
+import useWindowWidth from "@/hooks/useWindowWidth"; // hook import
 
-const Header = () => (
-  <header css={S.Header}>
-    <S.Container>
-      <S.LogoContainer>
-        <img src={poliText} alt="POLI-TEXT" />
-      </S.LogoContainer>
-      <div>
-        <S.StyledLink to="/login">로그인</S.StyledLink>
-        <S.StyledLink
-          to="/signup"
-          // css prop을 사용하여 직접 스타일링
-          css={css`
-            ${media.mobile} {
-              display: none;
-            }
-          `}
-        >
-          회원가입
-        </S.StyledLink>
-      </div>
-    </S.Container>
-  </header>
-);
+const Header = () => {
+  const width = useWindowWidth();
+  const isMobile = width <= 600;
+
+  return (
+    <header css={S.Header}>
+      <S.Container>
+        <S.LogoContainer>
+          <img src={isMobile ? poliSmText : poliText} alt="POLI-TEXT" />
+        </S.LogoContainer>
+        <div>
+          <S.StyledLink to="/login">로그인</S.StyledLink>
+          <S.StyledLink
+            to="/signup"
+            // css prop을 사용하여 직접 스타일링
+            css={css`
+              ${media.mobile} {
+                display: none;
+              }
+            `}
+          >
+            회원가입
+          </S.StyledLink>
+        </div>
+      </S.Container>
+    </header>
+  );
+};
 
 export default Header;
